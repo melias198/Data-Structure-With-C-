4b1307f6567f29fffbf9093f09994456fc31c3bb
melias198 <melias4600@gmail.com>
2023-01-12 12:13:26 +0600

    Practice Day 2(10.5)

--- Week 03/Practice Day 2(10.5)/2.cpp
+++ Week 03/Practice Day 2(10.5)/2.cpp
@@ -5,29 +5,23 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    vector<int>v(n);
-    for(int i=0;i<n;i++)
-
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
     {
-        cin>>v[i];
+        cin >> v[i];
     }
-    vector<int>cnt(n);
-    for(int i=0;i<n;i++)
+    vector<int> cnt(n);
+    for (int i = 0; i < n; i++)
     {
-      cnt[v[i]]++;
+        cnt[v[i]]++;
     }
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(cnt[i]>0)
-        { 
-            while(cnt[i]!=0) 
-            {
-                cout<<i<<" ";
-                cnt[i]--;
-            }
+        while (cnt[i]--)
+        {
+            cout << i << " ";
         }
     }
     return 0;
 }
-
